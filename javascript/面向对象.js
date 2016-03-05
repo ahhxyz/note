5c447cb155dfdff050aed34291a117d774189db8
wuxin <leng.yan.w@gmail.com>
2016-03-05 17:10:25 +0800

    面向对象写法

--- javascript/面向对象.js
+++ javascript/面向对象.js
@@ -1,13 +1,20 @@
-//使用构造函数来定义类的属性及属性的初始化
+/*
+ * 使用构造函数来定义类的属性及属性的初始化
+ */
 function Class(){
     this.property01 = '';
     this.property02 = '';
     //this.init() //实例化时要执行的方法
 }
+
+/*
+ * 使用原型来定义实例的共享成员，即方法和共享的成员变量
+ */
+
 Class.prototype.init = function(){
     this.property01 = 1;
 }
-//使用原型来定义实例的共享成员，即方法和共享的成员变量
+
 Class.prototype.say = function(){
     alert(this.property01);
 }
